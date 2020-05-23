2353633632f78e0c44aa5c0875e3d0ae4904d49c
Christian Lewis <christianlewis024@gmail.com>
2020-05-23 15:46:43 -0700

    login and reg working and sending token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,51 +1,97 @@
-const router = require("express").Router();
-const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const router = require("express").Router();
 
-const Users = require("../users/usersModel");
+const Users = require("../users/usersModel.js");
+const { isValid } = require("../users/users-service.js");
 
 router.post("/register", (req, res) => {
-  const user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10);
-  user.password = hash;
-  Users.insertUser(user)
-    .then((newUser) => {
-      user.id = newUser[0];
-      delete user.password;
-      const token = generateToken(user);
-      res.status(201).json({ user, token });
-    })
-    .catch((error) => {
-      res.status(500).json(error);
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 16;
+
+    // hash the password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save the user to the database
+    Users.add(credentials)
+      .then((user) => {
+        req.session.loggedIn === true;
+
+        res.status(201).json({ data: user });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
     });
+  }
 });
 
 router.post("/login", (req, res) => {
-  let { username, password } = req.body;
-  Users.findBy({ username })
-    .first()
-    .then((user) => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-        res.status(200).json({ username: user.username, token: token });
-      } else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    .catch((error) => {
-      res.status(500).json(error);
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Users.findBy({ username: username })
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          // we can save information about the client inside the session (req.session)
+          req.session.loggedIn = true;
+          req.session.user = user;
+          const token = createToken(user);
+
+          res.status(200).json({ message: "Welcome to our API", token });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password shoud be alphanumeric",
     });
+  }
 });
-function generateToken(user) {
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ message: "we could not log you out, try later please" });
+      } else {
+        res.status(204).end();
+      }
+    });
+  } else {
+    res.status(204).end();
+  }
+});
+function createToken(user) {
   const payload = {
-    userid: user.id,
+    sub: user.id,
     username: user.username,
+    role: user.role,
   };
+
+  const secret = process.env.JWT_SECRET || "keepitsecret,keepitsafe!";
+
   const options = {
-    expiresIn: "10000d",
+    expiresIn: "1d",
   };
-  const token = jwt.sign(payload, options);
-  return token;
+
+  return jwt.sign(payload, secret, options);
 }
 
 module.exports = router;
--- users/usersModel.js
+++ users/usersModel.js
@@ -1,13 +1,30 @@
 const db = require("../database/dbConfig");
 
 module.exports = {
-  insertUser,
+  add,
+  find,
   findBy,
+  findById,
 };
 
-function insertUser(user) {
-  return db("users").insert(user);
+function find() {
+  return db("users").select("id", "username");
 }
-function findBy(user) {
-  return db("users").where(user);
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+async function add(user) {
+  try {
+    const [id] = await db("users").insert(user, "id");
+
+    return findById(id);
+  } catch (error) {
+    throw error;
+  }
+}
+
+function findById(id) {
+  return db("users").where({ id }).first();
 }
